f694fb235cdb653eb576438e4752419fc1deafb7
Cencil <cencil@gmail.com>
2015-09-22 22:26:32 +0800

    Lesson 6 - Delete old data

--- app/src/main/java/com/example/android/sunshine/app/sync/SunshineSyncAdapter.java
+++ app/src/main/java/com/example/android/sunshine/app/sync/SunshineSyncAdapter.java
@@ -437,6 +437,11 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
                 cVVector.toArray(cvArray);
                 inserted = getContext().getContentResolver().bulkInsert(WeatherContract.WeatherEntry.CONTENT_URI, cvArray);
 
+                // delete old data so we don't build up and endless history
+                getContext().getContentResolver().delete(WeatherContract.WeatherEntry.CONTENT_URI,
+                        WeatherContract.WeatherEntry.COLUMN_DATE + " <= ?",
+                        new String[] {Long.toString(dayTime.getJulianDay(julianStartDay - 1, dayTime.gmtoff))});
+
                 notifyWeather();
             }
 
